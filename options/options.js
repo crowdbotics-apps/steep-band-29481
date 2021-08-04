38e8c41b546ceaa175523a8134eaccc6dd5db54b
Crowdbotics <team@crowdbotics.com>
2021-08-04 23:36:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "steep_band_29481Identifier",
+  url: "https://steep_band_29481Identifier.botics.co",
+  api: "https://steep_band_29481Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
